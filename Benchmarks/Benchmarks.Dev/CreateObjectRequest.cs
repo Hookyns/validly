f1678cbb35cd4141969f4b03e78fb9db1cecea54
Stepami <stepami@mail.ru>
2025-09-15 21:38:38 +0300

    test example

---
+++ Benchmarks/Benchmarks.Dev/CreateObjectRequest.cs
@@ -0,0 +1,23 @@
+using System.Runtime.CompilerServices;
+using Validly;
+using Validly.Validators;
+
+namespace Benchmarks.Dev;
+
+[Validatable]
+public partial record CreateObjectRequest
+{
+	[CustomValidation]
+	public string? IntegrationField { get; set; }
+
+	public async IAsyncEnumerable<ValidationMessage> ValidateIntegrationField(
+		IExternalService externalService,
+		[EnumeratorCancellation] CancellationToken ct = default)
+	{
+		if (IntegrationField is null)
+			yield break;
+		var isValid = await externalService.IsValidValue(IntegrationField, ct);
+		if (!isValid)
+			yield return new ValidationMessage("Integration field is invalid", "External.Integration");
+	}
+}
